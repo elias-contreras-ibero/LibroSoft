278e12bdec13e2da1735ed88902e140db06847d3
Elias Contreras <econtr25@estudiante.ibero.edu.co>
2025-11-29 19:28:28 -0500

    Diseño UI: Estructura HTML y estilos CSS base

---
+++ script.js
@@ -0,0 +1,13 @@
+// Simulación de Base de Datos (Array de Objetos)
+let library = [];
+
+// Referencias al DOM (HTML)
+const bookForm = document.getElementById('book-form');
+const bookList = document.getElementById('book-list');
+const searchInput = document.getElementById('search-bar');
+
+// Función de inicialización
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("LibroSoft v1.0 cargado correctamente.");
+    // Aquí cargaremos los libros más adelante
+});
